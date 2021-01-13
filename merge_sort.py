506f1d4890905feb5f5f3644585f464b37a9f171
Vincent <564177516@qq.com>
2021-01-13 18:01:18 +0800

    修改参数

--- merge_sort.py
+++ merge_sort.py
@@ -1,28 +1,28 @@
-def mergesort(list):
-    if len(list) <= 1:
-        return list
-    middle = len(list) // 2
-    left = mergesort(list[:middle])
-    right = mergesort(list[middle:])
-    list2 = []
+def mergesort(arr):
+    if len(arr) <= 1:
+        return arr
+    middle = len(arr) // 2
+    left = mergesort(arr[:middle])
+    right = mergesort(arr[middle:])
+    arr2 = []
     p1 = 0
     p2 = 0
     while p1 < len(left) and p2 < len(right):
         if left[p1] < right[p2]:
-            list2.append(left[p1])
+            arr2.append(left[p1])
             p1 += 1
         else:
-            list2.append(right[p2])
+            arr2.append(right[p2])
             p2 += 1
 
     if p1 == len(left):
-        list2.extend(right[p2:])
+        arr2.extend(right[p2:])
     else:
-        list2.extend(left[p1:])
+        arr2.extend(left[p1:])
 
-    return list2
+    return arr2
 
 
 if __name__ == '__main__':
-    a = [5, 4, 3, 2, 1]
+    a = [1,5,4,2,3]
     print(mergesort(a))
