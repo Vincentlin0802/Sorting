da65344ff863a138db25d13f50bdfa210a648f2d
Vincent <564177516@qq.com>
2021-01-13 16:47:59 +0800

    修改参数名

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,15 +1,15 @@
-def bubblesort(list):
-    for i in range(len(list)):
-        for j in range(0, len(list) - 1):
-            if len(list) < 2:
-                 break
-            elif list[j] > list[j + 1]:
-                 list[j], list[j + 1] = list[j + 1], list[j]
+def bubblesort(arr):
+    if len(arr) < 2:
+        return arr
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    return list
+    return arr
 
 
 if __name__ == '__main__':
-    a = [5,3,4,2,1]
+    a = [5, 3, 4, 2, 1]
     bubblesort(a)
     print(a)
