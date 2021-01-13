64d7c73c0845181c063bd3c99aa0b27c43ab13d6
Vincent <564177516@qq.com>
2021-01-13 18:00:11 +0800

    缩减时间复杂度

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@ def bubblesort(arr):
     if len(arr) < 2:
         return arr
     for i in range(len(arr)):
-        for j in range(len(arr) - 1):
+        for j in range(len(arr)-i-1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
@@ -10,6 +10,6 @@ def bubblesort(arr):
 
 
 if __name__ == '__main__':
-    a = [5, 3, 4, 2, 1]
+    a = [1,5,4,2,3]
     bubblesort(a)
     print(a)
