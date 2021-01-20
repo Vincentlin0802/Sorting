6a3e0ea2d5293bd1f76e3e6db48837de0c2c7c1c
Vincent <564177516@qq.com>
2021-01-20 14:14:03 +0800

    入参仅有数组

--- re_arrange.py
+++ re_arrange.py
@@ -1,11 +1,13 @@
 import random
-def rearrange(arr,left,right):
+def rearrange(arr):
+   left = arr[0]
+   right = arr[len(arr)-1]
    for i in range(0,len(arr),1):
-        num = random.randint(left,right)
+        num = random.randint(left, right)
         arr.append(num)
         arr.remove(num)
    return arr
 
 if __name__ == '__main__':
      a = [1,2,3,4,5,6]
-     print(rearrange(a,a[0],a[len(a)-1]))
+     print(rearrange(a))
