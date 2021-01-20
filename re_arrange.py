53d5b2260f6b2de7476fa980a573561f8b979e7a
Vincent <564177516@qq.com>
2021-01-20 11:41:50 +0800

    空间复杂度O(1)

--- re_arrange.py
+++ re_arrange.py
@@ -1,11 +1,10 @@
 import random
 def rearrange(arr):
-   arr2 = []
    for i in range(0,len(arr),1):
         num = random.choice(arr)
-        arr2.append(num)
+        arr.append(num)
         arr.remove(num)
-   return arr2
+   return arr
 
 if __name__ == '__main__':
      a = [1,2,3,4,5,6]
