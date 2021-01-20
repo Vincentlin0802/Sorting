a3aa0d9ccca4f8ccebc7654271d3e51dad2efa7f
Vincent <564177516@qq.com>
2021-01-20 14:01:12 +0800

    use random.randint

--- re_arrange.py
+++ re_arrange.py
@@ -1,11 +1,11 @@
 import random
-def rearrange(arr):
+def rearrange(arr,left,right):
    for i in range(0,len(arr),1):
-        num = random.choice(arr)
+        num = random.randint(left,right)
         arr.append(num)
         arr.remove(num)
    return arr
 
 if __name__ == '__main__':
      a = [1,2,3,4,5,6]
-     print(rearrange(a))
+     print(rearrange(a,a[0],a[len(a)-1]))
